e1e634a6c7ef87076425574cc2ec7b8e7e77b1da
Evanilson Pereira <pevanilson99@gmail.com>
2024-01-23 11:12:22 -0300

    Creating server/routes folder

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-
-app.get('/', (req, res) => {
-    res.send('Hi!');
-});
+// Home page / Grabbing the router from main js file in server/routes/main.js
+app.use('/', require('./server/routes/main'));
 
 app.listen(PORT, () => console.log(`Up and running on PORT ${PORT}`));
