fcaadc98fedb0edf22d0a7a302ddbdec0de6ff1d
Evanilson Pereira <pevanilson99@gmail.com>
2024-01-26 11:12:03 -0300

    Pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,22 +3,55 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 // GET / HOME
+// router.get('', async (req, res) => {
+//     const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple Blog created with NodeJs, Express & MongoDb."
+//   }
+
+//   try {
+//     const data = await Post.find();
+//     res.render('index', { locals, data });
+//   } 
+
+//   catch (error) {
+//     console.log(error);
+//   }
+
+// });
+
 router.get('', async (req, res) => {
-    const locals = {
-    title: "NodeJs Blog",
-    description: "Simple Blog created with NodeJs, Express & MongoDb."
-  }
-
-  try {
-    const data = await Post.find();
-    res.render('index', { locals, data });
-  } 
-
-  catch (error) {
-    console.log(error);
-  }
-
-});
+    try {
+      const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+      }
+  
+      let perPage = 5;
+      let page = req.query.page || 1;
+  
+      const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+  
+      const count = await Post.countDocuments({});
+      const nextPage = parseInt(page) + 1;
+      const hasNextPage = nextPage <= Math.ceil(count / perPage);
+  
+      res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+      });
+  
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
 
 router.get('/about', (req, res) => {
     res.render('about');
