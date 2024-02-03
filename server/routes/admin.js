a93bf88e9c0f06bf589054e4783835491fb3115e
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-03 11:13:09 -0300

    Add-post route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -92,6 +92,26 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
+// GET - ADMIN CREATE NEW POST
+router.get('/add-post', authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: 'Add Post',
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.'
+    }
+
+    const data = await Post.find();
+    res.render('admin/add-post', {
+      locals,
+      layout: adminLayout
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
 // GET - ADMIN DASHBOARD
 router.get('/dashboard', authMiddleware, async (req, res) => {
   try {
