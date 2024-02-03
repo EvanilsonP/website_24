7acbdfd158b82979193b79384640cb53ca460129
Evanilson Pereira <pevanilson99@gmail.com>
2024-02-03 11:24:03 -0300

    Edit

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,50 +5,52 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 const adminLayout = '../views/layouts/admin';
 const jwtSecret = process.env.JWT_SECRET;
 
+/**
+ * 
+ * Check Login
+*/
+const authMiddleware = (req, res, next ) => {
+  const token = req.cookies.token;
 
-// GET / Admin login page
+  if(!token) {
+    return res.status(401).json( { message: 'Unauthorized'} );
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch(error) {
+    res.status(401).json( { message: 'Unauthorized'} );
+  }
+}
+
+
+/**
+ * GET /
+ * Admin - Login Page
+*/
 router.get('/admin', async (req, res) => {
   try {
     const locals = {
-        title: "Admin",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
-      }
-
-    res.render('admin/index', { locals, layout: adminLayout});
-  } 
-
-  catch (error) {
-    console.log(error);
-  }
-
-});
-
-// POST - Admin - Register
-router.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    try {
-      const user = await User.create({ username, password:hashedPassword });
-      res.status(201).json({ message: 'User Created', user });
-    } catch (error) {
-      if(error.code === 11000) {
-        res.status(409).json({ message: 'User already in use'});
-      }
-      res.status(500).json({ message: 'Internal server error'})
+      title: "Admin",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
     }
 
+    res.render('admin/index', { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
 });
 
-// POST / Admin - Check Login
+
+/**
+ * POST /
+ * Admin - Check Login
+*/
 router.post('/admin', async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -56,63 +58,29 @@ router.post('/admin', async (req, res) => {
     const user = await User.findOne( { username } );
 
     if(!user) {
-      return res.status(401).json( { message: 'User not found' } );
+      return res.status(401).json( { message: 'Invalid credentials' } );
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if(!isPasswordValid) {
-      return res.status(401).json( { message: 'Invalid password' } );
+      return res.status(401).json( { message: 'Invalid credentials' } );
     }
 
-    // Saving token to the cookie
-  const token = jwt.sign({ userId: user._id}, jwtSecret);
-  res.cookie('token', token, { httpOnly: true });
-  res.redirect('/dashboard');
+    const token = jwt.sign({ userId: user._id}, jwtSecret );
+    res.cookie('token', token, { httpOnly: true });
+    res.redirect('/dashboard');
 
   } catch (error) {
     console.log(error);
   }
 });
 
-// Check Login
-const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token;
 
-  if(!token) return res.status(401).json( { message: 'Unauthorized'} );
-
-  try {
-    const decoded = jwt.verify(token, jwtSecret);
-    req.userId = decoded.userId;
-    next();
-  } 
-  
-  catch (error) {
-    res.status(401).json( { message: 'Unauthorized'} );
-  }
-};
-
-// GET - ADMIN CREATE NEW POST
-router.get('/add-post', authMiddleware, async (req, res) => {
-  try {
-    const locals = {
-      title: 'Add Post',
-      description: 'Simple Blog created with NodeJs, Express & MongoDb.'
-    }
-
-    const data = await Post.find();
-    res.render('admin/add-post', {
-      locals,
-      layout: adminLayout
-    });
-
-  } catch (error) {
-    console.log(error);
-  }
-
-});
-
-// GET - ADMIN DASHBOARD
+/**
+ * GET /
+ * Admin Dashboard
+*/
 router.get('/dashboard', authMiddleware, async (req, res) => {
   try {
     const locals = {
@@ -133,6 +101,127 @@ router.get('/dashboard', authMiddleware, async (req, res) => {
 
 });
 
+
+/**
+ * GET /
+ * Admin - Create New Post
+*/
+router.get('/add-post', authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: 'Add Post',
+      description: 'Simple Blog created with NodeJs, Express & MongoDb.'
+    }
+
+    const data = await Post.find();
+    res.render('admin/add-post', {
+      locals,
+      layout: adminLayout
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * POST /
+ * Admin - Create New Post
+*/
+router.post('/add-post', authMiddleware, async (req, res) => {
+  try {
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body
+      });
+
+      await Post.create(newPost);
+      res.redirect('/dashboard');
+    } catch (error) {
+      console.log(error);
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+/**
+ * GET /
+ * Admin - Create New Post
+*/
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    const locals = {
+      title: "Edit Post",
+      description: "Free NodeJs User Management System",
+    };
+
+    const data = await Post.findOne({ _id: req.params.id });
+
+    res.render('admin/edit-post', {
+      locals,
+      data,
+      layout: adminLayout
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * PUT /
+ * Admin - Create New Post
+*/
+router.put('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now()
+    });
+
+    res.redirect(`/edit-post/${req.params.id}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * POST /
+ * Admin - Register
+*/
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password:hashedPassword });
+      res.status(201).json({ message: 'User Created', user });
+    } catch (error) {
+      if(error.code === 11000) {
+        res.status(409).json({ message: 'User already in use'});
+      }
+      res.status(500).json({ message: 'Internal server error'})
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
 module.exports = router;
